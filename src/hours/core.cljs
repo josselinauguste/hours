cdbe50293b0f799743b6cac894491518739461ee
Josselin Auguste <jauguste@iblop.net>
2020-08-29 16:03:39 +0200

    Add reagent

--- src/hours/core.cljs
+++ src/hours/core.cljs
@@ -1,4 +1,12 @@
-(ns hours.core)
+(ns hours.core
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as rdom]))
+
+(defn hours-app []
+  [:div
+   [:h1 "Hours"]
+   ])
 
 (defn main! []
-)
+  (rdom/render [hours-app] (js/document.getElementById "app")))
