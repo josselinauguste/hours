c8824d9bc17511937d1ddd9df824d91a2ba73850
Josselin Auguste <jauguste@iblop.net>
2020-09-06 22:21:49 +0200

    Basic UI styling

--- src/hours/core.cljs
+++ src/hours/core.cljs
@@ -6,15 +6,25 @@
     [cljs-time.format :as time-format]
     [hours.calendar :as cal]))
 
+(defn day [name]
+  [:p.inline-block.text-sm.text-center name [:br] [:input.w-4.text-center {:type "text" :value "0"}]])
+
 (defn week [week]
-  [:div [:p (time-format/unparse (time-format/formatter "d/MM") (first (cal/days-of-week week)))]])
+  [:div.mt-2
+   [:p.block (time-format/unparse (time-format/formatter "d/MM") (first (cal/days-of-week week)))]
+   [:div.grid.grid-cols-5
+    (day "mon")
+    (day "tue")
+    (day "wed")
+    (day "thu")
+    (day "fri")]])
 
 (defn month [for-day]
-  (into [:div] (for [w (reverse (cal/weeks for-day))] (week w))))
+  (into [:div.mt-4] (for [w (reverse (cal/weeks for-day))] (week w))))
 
 (defn hours-app []
-  [:div
-    [:h1 "Hours"]
+  [:div.container.p-4.mx-auto.md:max-w-lg.text-gray-900
+    [:h1.mt-4.text-2xl "Hours"]
     (month (time/today))
     ])
 
